81ee5ce93fd8d89c0d06c66f768a8f3c03a520a0
PaulAguirre <PaulAguirre@users.noreply.github.com>
2016-11-21 10:03:21 -0500

    Update and rename vectoresGitHub/src/vectoresgithub/VectoresGitHub.java to  vectoresGitHub/ src/ vectoresgithub/VectoresGitHub.java

---
+++ vectoresGitHub/ src/ vectoresgithub/VectoresGitHub.java
@@ -0,0 +1,16 @@
+package vectoresgithub;
+
+public class VectoresGitHub {
+
+    public static void main(String[] args) {
+        //2222
+        int A[];
+        int B[];
+        int pares = 0;
+          A = new int[pares];
+        int impares = 0;
+          B = new int [impares];
+          int suma=0;
+    }
+    
+}
